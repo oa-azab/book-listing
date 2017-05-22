f4d1653ae47d4a2d09f1a8578fd02be478e3c8fa
Omar <oa.azab@gmail.com>
2017-05-22 23:03:02 +0200

    Loading Indicator and Internet connection

--- app/src/main/java/me/azab/oa/booklisting/MainActivity.java
+++ app/src/main/java/me/azab/oa/booklisting/MainActivity.java
@@ -1,5 +1,8 @@
 package me.azab.oa.booklisting;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mEditText;
     Button mButton;
     TextView mEmptyView;
+    ProgressBar mLoadingIndicator;
     BooksAdapter mBooksAdapter;
     List<Book> mBooks = new ArrayList<>();
 
@@ -35,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         mEditText = (EditText) findViewById(R.id.edit_text_search);
         mButton = (Button) findViewById(R.id.btn_search);
         mEmptyView = (TextView) findViewById(R.id.empty_view);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
+
+        // Set loading indicator visiblity gone
+        mLoadingIndicator.setVisibility(View.GONE);
 
         // Initialize books adapter
         mBooksAdapter = new BooksAdapter(this, mBooks);
@@ -49,14 +58,32 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String searchParam = mEditText.getText().toString();
-                searchParam = searchParam.replace(' ','+');
-                new BooksAsyncTask().execute(BASE_QUERY_URL+searchParam);
+                // Get a reference to the ConnectivityManager to check state of network connectivity
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                // Get details on the currently active default data network
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                // If there is a network connection, fetch data
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    String searchParam = mEditText.getText().toString();
+                    searchParam = searchParam.replace(' ', '+');
+                    new BooksAsyncTask().execute(BASE_QUERY_URL + searchParam);
+                } else {
+                    mEmptyView.setText(getString(R.string.no_connection));
+                }
             }
         });
     }
 
     private class BooksAsyncTask extends AsyncTask<String, Void, List<Book>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+            mEmptyView.setVisibility(View.GONE);
+        }
 
         @Override
         protected List<Book> doInBackground(String... urls) {
@@ -70,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
             mBooksAdapter.clear();
             mBooksAdapter.addAll(books);
+            mLoadingIndicator.setVisibility(View.GONE);
         }
     }
 }
